1c38f009c8d05ed1a03ec9880887d7848bfed585
Ashraf1551 <mdashrafgmhs@gmail.com>
2025-11-24 23:34:22 +0600

    home section completed

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Slider from "../component/Slider";
+import PopularSection from "../component/PopularSection";
+
+const Home = () => {
+  return (
+    <div>
+      <title>Home</title>
+      <Slider></Slider>
+      <PopularSection></PopularSection>
+    </div>
+  );
+};
+
+export default Home;
